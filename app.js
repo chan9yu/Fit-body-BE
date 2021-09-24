9b48df8566a37cecd3f894bb296a63abc28e790f
ChangyuYeo <wry5533@gmail.com>
2021-09-24 21:30:07 +0900

    환경변수 설정

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const { mongoURI } = require('./config/key')
+
+const { User } = require('./models/User')
 
 const app = express()
 const port = 3000
 
-mongoose.connect()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+mongoose
+	.connect(mongoURI)
+	.then(() => console.log('몽구스 연결 완료!'))
+	.catch(err => console.error(err))
 
 app.get('/', (req, res) => res.send('예아'))
+
+app.post('/register', (req, res) => {
+	const user = new User(req.body)
+	user.save((err, userInfo) => {
+		if (err) return res.json({ success: false, err })
+		return res.status(200).json({ success: true })
+	})
+})
 
 app.listen(port, () => console.log(`express port ${port} 으로 시작!`))
