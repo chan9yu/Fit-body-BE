8e0897b5d62d8e149744032036d910abffc566ec
Chan Gyu <80776262+changyuyeo@users.noreply.github.com>
2021-10-21 23:06:31 +0900

    상품 더보기, 카테고리 기능 구현

--- router/product.js
+++ router/product.js
@@ -42,4 +42,24 @@ router.post('/', async (req, res) => {
 	}
 })
 
+router.post('/products', async (req, res) => {
+	// 더보기 기능
+	const skip = req.body.skip ? parseInt(req.body.skip) : 0
+	const limit = req.body.limit ? parseInt(req.body.limit) : 20
+	// 카테고리 분류 기능 구현
+	const { categorys, subCategorys } = req.body
+	console.log(categorys)
+	console.log(subCategorys)
+	try {
+		const products = await Product.find(categorys ? { categorys } : undefined)
+			.find(subCategorys ? { subCategorys } : undefined)
+			.skip(skip)
+			.limit(limit)
+		return res.status(200).json({ products, postSize: products.length })
+	} catch (error) {
+		console.error(error)
+		return res.status(500).send({ error })
+	}
+})
+
 module.exports = router
